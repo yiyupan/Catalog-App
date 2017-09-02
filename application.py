16f6c860d67b3b113eaa149fcb4f8813520f67f1
yiyupan <yiyupan@umich.edu>
2017-09-02 19:13:10 -0400

    Add files via upload

---
+++ application.py
@@ -0,0 +1,343 @@
+from flask import Flask, render_template, request, redirect
+from flask import jsonify, url_for, flash
+
+
+from sqlalchemy import create_engine, asc
+from sqlalchemy.orm import sessionmaker
+from database_setup import Category, Base, Item, User
+
+
+# New imports for this step
+from flask import session as login_session
+import random
+import string
+
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+import httplib2
+import json
+from flask import make_response
+import requests
+app = Flask(__name__)
+
+CLIENT_ID = json.loads(
+    open('client_secrets.json', 'r').read())['web']['client_id']
+# APPLICATION_NAME = "Catalog Application"
+
+# Connect to Database and create database session
+engine = create_engine('sqlite:///categoryitems.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+
+# Create anti-forgery state token
+@app.route('/login')
+def showLogin():
+    state = ''.join(random.choice(string.ascii_uppercase + string.digits)
+                    for x in xrange(32))
+    login_session['state'] = state
+    return render_template('login.html', STATE=state)
+
+
+@app.route('/gconnect', methods=['POST'])
+def gconnect():
+    # Validate state token
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state parameter.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    # Obtain authorization code
+    code = request.data
+
+    try:
+        # Upgrade the authorization code into a credentials object
+        oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+        oauth_flow.redirect_uri = 'postmessage'
+        credentials = oauth_flow.step2_exchange(code)
+    except FlowExchangeError:
+        response = make_response(
+            json.dumps('Failed to upgrade the authorization code.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Check that the access token is valid.
+    access_token = credentials.access_token
+    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
+           % access_token)
+    h = httplib2.Http()
+    result = json.loads(h.request(url, 'GET')[1])
+    # If there was an error in the access token info, abort.
+    if result.get('error') is not None:
+        response = make_response(json.dumps(result.get('error')), 500)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Verify that the access token is used for the intended user.
+    gplus_id = credentials.id_token['sub']
+    if result['user_id'] != gplus_id:
+        response = make_response(
+            json.dumps("Token's user ID doesn't match given user ID."), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Verify that the access token is valid for this app.
+    if result['issued_to'] != CLIENT_ID:
+        response = make_response(
+            json.dumps("Token's client ID does not match app's."), 401)
+        print "Token's client ID does not match app's."
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Check to see if user is already logged in
+    stored_access_token = login_session.get('access_token')
+    stored_gplus_id = login_session.get('gplus_id')
+    if stored_access_token is not None and gplus_id == stored_gplus_id:
+        response = make_response(
+                    json.dumps('Current user is already connected.'),
+                    200)
+        response.headers['Content-Type'] = 'application/json'
+
+    # Store the access token in the session for later use.
+    login_session['access_token'] = credentials.access_token
+    login_session['gplus_id'] = gplus_id
+
+    # Get user info
+    userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+    params = {'access_token': credentials.access_token, 'alt': 'json'}
+    answer = requests.get(userinfo_url, params=params)
+
+    data = answer.json()
+
+    login_session['username'] = data['name']
+    login_session['picture'] = data['picture']
+    login_session['email'] = data['email']
+
+    # See if a user exists, if it doesn't make a new one
+    user_id = getUserID(login_session['email'])
+    if not user_id:
+        user_id = createUser(login_session)
+    login_session['user_id'] = user_id
+
+    output = ''
+    output += '<h1>Welcome, '
+    output += login_session['username']
+    output += '!</h1>'
+    output += '<img src="'
+    output += login_session['picture']
+    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;'
+    output += '-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+    flash("you are now logged in as %s" % login_session['username'])
+    print "done!"
+    return output
+
+
+# User Helper Functions
+
+
+def createUser(login_session):
+    newUser = User(name=login_session['username'], email=login_session[
+                   'email'], picture=login_session['picture'])
+    session.add(newUser)
+    session.commit()
+    user = session.query(User).filter_by(email=login_session['email']).one()
+    return user.id
+
+
+def getUserInfo(user_id):
+    user = session.query(User).filter_by(id=user_id).one()
+    return user
+
+
+def getUserID(email):
+    try:
+        user = session.query(User).filter_by(email=email).one()
+        return user.id
+    except:
+        return None
+
+
+@app.route('/gdisconnect')
+def gdisconnect():
+    access_token = login_session.get('access_token')
+    if access_token is None:
+        print 'Access Token is None'
+        response = make_response(
+                    json.dumps('Current user not connected.'),
+                    401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    print 'In gdisconnect access token is %s', access_token
+    print 'User name is: '
+    print login_session['username']
+    url = ('https://accounts.google.com/o/oauth2' +
+           '/revoke?token=%s' % login_session['access_token'])
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[0]
+    print 'result is '
+    print result
+
+    if result['status'] == '200':
+        del login_session['access_token']
+        del login_session['gplus_id']
+        del login_session['username']
+        del login_session['email']
+        del login_session['picture']
+        response = make_response(json.dumps('Successfully disconnected.'),
+                                 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    else:
+        response = make_response(
+                    json.dumps('Failed to revoke token for given user.', 400))
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+
+# JSON APIs to view Category Information
+@app.route('/catalog/<string:category_name>/JSON')
+def categoryItemJSON(category_name):
+    category = session.query(Category).filter_by(name=category_name).one()
+    items = session.query(Item).filter_by(category_id=category.id).all()
+    return jsonify(Items=[i.serialize for i in items])
+
+
+@app.route('/catalog/<string:category_name>/<string:item_name>/JSON')
+def itemJSON(category_name, item_name):
+    item_info = session.query(Item).filter_by(name=item_name).one()
+    return jsonify(item_info=item_info.serialize)
+
+
+@app.route('/catalog/JSON')
+def restaurantsJSON():
+    categories = session.query(Category).all()
+    return jsonify(categories=[r.serialize for r in categories])
+
+
+# Show all categories
+@app.route('/')
+@app.route('/catalog/')
+def showCategories():
+    categories = session.query(Category).order_by(asc(Category.name))
+    items = session.query(Item).order_by(asc(Item.id))
+    if 'username' not in login_session:
+        return render_template('publiccategory.html', categories=categories,
+                               items=items)
+    else:
+        creator = getUserInfo(login_session['user_id'])
+        return render_template('category.html', categories=categories,
+                               items=items, creator=creator)
+
+
+# Show all items
+@app.route('/catalog/<string:category_name>/items/')
+def showItems(category_name):
+    categories = session.query(Category).order_by(asc(Category.name)).all()
+    category = session.query(Category).filter_by(name=category_name).one()
+    items = session.query(Item).filter_by(category_id=category.id).all()
+    if 'username' not in login_session:
+        return render_template('publicitems.html',
+                               items=items,
+                               categories=categories,
+                               category=category)
+    else:
+        creator = getUserInfo(login_session['user_id'])
+        return render_template('items.html',
+                               items=items,
+                               categories=categories,
+                               category=category,
+                               creator=creator)
+
+
+# Show item info
+@app.route('/catalog/<string:category_name>/<string:item_name>/')
+def showInfo(category_name, item_name):
+    category = session.query(Category).filter_by(name=category_name).all()
+    item = session.query(Item).filter_by(name=item_name).one()
+    creator = getUserInfo(item.user_id)
+    if 'username' not in login_session:
+        return render_template('publicinfo.html', item=item)
+    else:
+        return render_template('info.html', item=item, creator=creator)
+
+
+# Create a new item
+@app.route('/catalog/new/', methods=['GET', 'POST'])
+def newItem():
+    if 'username' not in login_session:
+        return redirect('/login')
+    if request.method == 'POST':
+        category = session.query(Category).filter_by(
+                                        name=request.form['category']).one()
+        newItem = Item(name=request.form['name'],
+                       description=request.form['description'],
+                       category_id=category.id,
+                       user_id=login_session['user_id'])
+        session.add(newItem)
+        session.commit()
+        flash('New Item %s Successfully Created!' % (newItem.name))
+        return redirect(url_for('showCategories'))
+    else:
+        creator = getUserInfo(login_session['user_id'])
+        return render_template('new.html', creator=creator)
+
+
+# Edit an item
+@app.route('/catalog/<string:item_name>/edit/', methods=['GET', 'POST'])
+def editItem(item_name):
+    editedItem = session.query(Item).filter_by(name=item_name).one()
+    if 'username' not in login_session:
+        return redirect('/login')
+    if editedItem.user_id != login_session['user_id']:
+        return """
+         <script>function myFunction() {alert('You are not
+         authorized to edit this item. Please create your own item in
+         order to edit.');}</script><body onload='myFunction()''>
+         """
+    if request.method == 'POST':
+        if request.form['name']:
+            editedItem.name = request.form['name']
+        if request.form['description']:
+            editedItem.description = request.form['description']
+        if request.form['category']:
+            category = session.query(Category).filter_by(
+                        name=request.form['category']).one()
+            editedItem.category_id = category.id
+        session.add(editedItem)
+        session.commit()
+        flash('Item Successfully Edited')
+        return redirect(url_for('showCategories'))
+    else:
+        creator = getUserInfo(editedItem.user_id)
+        return render_template('edit.html', item=editedItem, creator=creator)
+
+
+# Delete an item
+@app.route('/catalog/<string:item_name>/delete/', methods=['GET', 'POST'])
+def deleteItem(item_name):
+    itemToDelete = session.query(Item).filter_by(name=item_name).one()
+    if 'username' not in login_session:
+        return redirect('/login')
+    if itemToDelete.user_id != login_session['user_id']:
+        return """
+         <script>function myFunction() {alert('You are not
+         authorized to delete this item. Please create your own item
+         in order to delete.');}</script><body onload='myFunction()''>
+         """
+    if request.method == 'POST':
+        session.delete(itemToDelete)
+        session.commit()
+        flash('Item Successfully Deleted')
+        return redirect(url_for('showCategories'))
+    else:
+        creator = getUserInfo(itemToDelete.user_id)
+        return render_template('delete.html',
+                               item=itemToDelete,
+                               creator=creator)
+
+
+if __name__ == '__main__':
+    app.secret_key = 'super_secret_key'
+    app.debug = True
+    app.run(host='0.0.0.0', port=8000)
---
+++ lotsofitems.py
@@ -0,0 +1,183 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Category, Base, Item, User
+
+engine = create_engine('sqlite:///categoryitems.db')
+# Bind the engine to the metadata of the Base class so that the
+# declaratives can be accessed through a DBSession instance
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+# A DBSession() instance establishes all conversations with the database
+# and represents a "staging zone" for all the objects loaded into the
+# database session object. Any change made against the objects in the
+# session won't be persisted into the database until you call
+# session.commit(). If you're not happy about the changes, you can
+# revert all of them back to the last commit by calling
+# session.rollback()
+session = DBSession()
+
+
+# Create dummy user
+User1 = User(name="Yiyu Pan", email="yiyupan666@gmail.com",
+             picture='https://pbs.twimg.com/profile_images/' +
+             '2671170543/18debd694829ed78203a5a36dd364160_400x400.png')
+session.add(User1)
+session.commit()
+
+# Items for Soccer
+category1 = Category(name="Soccer", user_id=User1.id)
+
+session.add(category1)
+session.commit()
+
+
+item1 = Item(name="Jersey",
+             description="""
+             The Laws of the Game set out the basic equipment
+              which must be worn by all players in Law 4: The Players'
+              Equipment.
+              Five separate items are specified: shirt
+              (also known as a jersey),
+              shorts, socks also known as stockings), footwear and shin pads.
+             """,
+             category_id=category1.id,
+             user_id=User1.id)
+session.add(item1)
+session.commit()
+
+item2 = Item(name="Soccer Cleats",
+             description="""Soccer cleats, most of the time a soccer shoe
+              is a firm ground soccer shoe. Firm ground is the classic soccer
+              shoe with cleats/studs designed to provide traction and stability
+              on most natural grass, outdoor soccer fields""",
+             category_id=category1.id,
+             user_id=User1.id)
+session.add(item2)
+session.commit()
+
+item3 = Item(name="Two shinguards",
+             description="""Today, there are a two basic types of shin guards
+              used in association football:
+              slip-in shin guards and ankle shin guards.""",
+             category_id=category1.id,
+             user_id=User1.id)
+session.add(item3)
+session.commit()
+
+item4 = Item(name="Shinguards",
+             description="""A shin guard or shin pad is a piece of
+              equipment worn on the front of a player's shin to protect
+              them from injury.""",
+             category_id=category1.id,
+             user_id=User1.id)
+session.add(item4)
+session.commit()
+
+# Items for Hockey
+category2 = Category(name="Hockey", user_id=User1.id)
+
+session.add(category2)
+session.commit()
+
+
+item5 = Item(name="Stick",
+             description="""
+             The stick used in bandy
+              should be made of an approved material such as wood
+              or a similar material and should not contain any
+              metal or sharp parts which can hurt the surrounding players.
+             """,
+             category_id=category2.id,
+             user_id=User1.id)
+session.add(item5)
+session.commit()
+
+# Items for Snowboarding
+category3 = Category(name="Snowboarding", user_id=User1.id)
+
+session.add(category3)
+session.commit()
+
+
+item6 = Item(name="Goggles",
+             description="""
+             Goggles, or safety glasses, are forms of protective
+              eyewear that usually enclose or protect the area
+              surrounding the eye in order to prevent
+              particulates, water or chemicals from striking
+              the eyes.
+             """,
+             category_id=category3.id,
+             user_id=User1.id)
+session.add(item6)
+session.commit()
+
+item7 = Item(name="Snowboard",
+             description="""
+             Snowboards are boards that are usually the width of one's
+              foot longways, with the ability to glide on snow.
+              Snowboards are differentiated from monoskis
+              by the stance of the user
+             """,
+             category_id=category3.id,
+             user_id=User1.id)
+session.add(item7)
+session.commit()
+
+# Items for Frisbee
+category4 = Category(name="Frisbee", user_id=1)
+
+session.add(category4)
+session.commit()
+
+
+item8 = Item(name="Frisbee",
+             description="""
+             A frisbee is a gliding toy or sporting item that is generally
+              plastic and roughly 20 to 25 centimetres in diameter with
+              a lip,[1] used recreationally and competitively for throwing
+              and catching, for example, in flying disc games.
+             """,
+             category_id=category4.id,
+             user_id=User1.id)
+session.add(item8)
+session.commit()
+
+# Items for Baseball
+category5 = Category(name="Baseball", user_id=User1.id)
+
+session.add(category5)
+session.commit()
+
+
+item9 = Item(name="Bat",
+             description="""
+                A baseball bat is a smooth wooden or metal club used in
+                 the sport of baseball to hit the ball after it is thrown
+                 by the pitcher.
+                """,
+             category_id=category5.id,
+             user_id=User1.id)
+session.add(item9)
+session.commit()
+
+# Category of Rock Climbing
+category6 = Category(name="Rock Climbing", user_id=User1.id)
+
+session.add(category6)
+session.commit()
+
+# Category of Foosball
+category7 = Category(name="Foosball", user_id=User1.id)
+
+session.add(category7)
+session.commit()
+
+# Category of Skating
+category8 = Category(name="Skating", user_id=User1.id)
+
+session.add(category8)
+session.commit()
+
+print "added items"
